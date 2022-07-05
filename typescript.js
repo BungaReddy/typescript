481d7c612f1faa2e8f69c056d504b8ef5ebb0200
BungaReddy <bchandravamsireddy@gmail.com>
2022-07-05 11:34:10 +0530

    update file

---
+++ typescript.js
@@ -0,0 +1,5 @@
+function Numbers(a, b) {
+    return a + b;
+}
+var sum = Numbers(1, 3);
+console.log('sum of the numbers is :' + sum);
---
+++ typescript.ts
@@ -0,0 +1,5 @@
+function Numbers(a:number,b:number){
+    return a+b;
+}
+var sum: number = Numbers(1,3)
+console.log('sum of the numbers is :'+sum);
